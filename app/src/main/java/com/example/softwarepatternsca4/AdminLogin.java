12bdfe6fb2c470b880259fd804fb17cf400a76a7
PeterLargey <C18513633@mytudublin.ie>
2022-03-29 10:43:10 +0100

    Initial Commit

---
+++ app/src/main/java/com/example/softwarepatternsca4/AdminLogin.java
@@ -0,0 +1,87 @@
+package com.example.softwarepatternsca4;
+
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
+
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.firestore.DocumentSnapshot;
+import com.google.firebase.firestore.FirebaseFirestore;
+import com.google.firebase.firestore.QuerySnapshot;
+
+import java.util.Map;
+
+public class AdminLogin extends AppCompatActivity {
+
+    private FirebaseFirestore db;
+    private Map<String, Object> dbData;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_admin_login);
+
+        db = FirebaseFirestore.getInstance();
+        EditText username = findViewById(R.id.adminUserName);
+        EditText password = findViewById(R.id.adminPassword);
+        Button login = findViewById(R.id.adminLogin);
+
+        login.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                String usernameString = username.getText().toString();
+                String passwordString = password.getText().toString();
+                if(usernameString.isEmpty() || passwordString.isEmpty()){
+                    Toast.makeText(getApplicationContext(), "All Fields are required", Toast.LENGTH_LONG).show();
+                }else{
+                    db.collection("Admin").get().addOnCompleteListener(new OnCompleteListener<QuerySnapshot>() {
+                        @Override
+                        public void onComplete(@NonNull Task<QuerySnapshot> task) {
+                           if(task.isSuccessful()){
+                               for(DocumentSnapshot doc : task.getResult()){
+                                   dbData = doc.getData();
+                               }
+
+                               Object[] values = dbData.values().toArray();
+                               String username = values[0].toString();
+                               String password = values[1].toString();
+                               if(username.equalsIgnoreCase(usernameString) && password.equalsIgnoreCase(passwordString)){
+                                   Intent i = new Intent(AdminLogin.this, AdminMain.class);
+                                   startActivity(i);
+                               } else{
+                                   Toast.makeText(getApplicationContext(), "Login Failed, Account doesn't exist", Toast.LENGTH_LONG).show();
+                               }
+
+                           }
+                        }
+                    });
+                }
+            }
+        });
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.back_to_customer_login, menu);
+        return super.onCreateOptionsMenu(menu);
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(@NonNull MenuItem item) {
+        int id = item.getItemId();
+        if(id == R.id.customer_login){
+            Intent i = new Intent(AdminLogin.this, MainActivity.class);
+            startActivity(i);
+        }
+        return super.onOptionsItemSelected(item);
+    }
+}
---
+++ app/src/main/java/com/example/softwarepatternsca4/MainActivity.java
@@ -0,0 +1,84 @@
+package com.example.softwarepatternsca4;
+
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
+
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
+
+public class MainActivity extends AppCompatActivity {
+
+    private FirebaseAuth mAuth;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        getSupportActionBar().setTitle("Peter's Store");
+        mAuth = FirebaseAuth.getInstance();
+
+        EditText email = findViewById(R.id.loginEmail);
+        EditText password = findViewById(R.id.loginPassword);
+        Button login = findViewById(R.id.login);
+        Button register = findViewById(R.id.navigateToRegister);
+
+        login.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                String emailString = email.getText().toString();
+                String passwordString = password.getText().toString();
+                if(emailString.isEmpty() || passwordString.isEmpty()){
+                    Toast.makeText(getApplicationContext(), "All Fields are required", Toast.LENGTH_LONG).show();
+                }else{
+                    mAuth.signInWithEmailAndPassword(emailString, passwordString).addOnCompleteListener(new OnCompleteListener<AuthResult>() {
+                        @Override
+                        public void onComplete(@NonNull Task<AuthResult> task) {
+                            if(task.isSuccessful()){
+                                Toast.makeText(getApplicationContext(), "Login Successful", Toast.LENGTH_LONG).show();
+                                Intent i = new Intent(MainActivity.this, Store.class);
+                                startActivity(i);
+                            }else{
+                                Toast.makeText(getApplicationContext(), "Login Failed, Account doesn't exist", Toast.LENGTH_LONG).show();
+                            }
+                        }
+                    });
+                }
+            }
+        });
+
+       register.setOnClickListener(new View.OnClickListener() {
+           @Override
+           public void onClick(View view) {
+               Intent i = new Intent(MainActivity.this, Register.class);
+               startActivity(i);
+           }
+       });
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.admin_login_menu, menu);
+        return super.onCreateOptionsMenu(menu);
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(@NonNull MenuItem item) {
+        int id = item.getItemId();
+        if(id == R.id.admin_login){
+            Intent i = new Intent(MainActivity.this, AdminLogin.class);
+            startActivity(i);
+        }
+        return super.onOptionsItemSelected(item);
+    }
+}
